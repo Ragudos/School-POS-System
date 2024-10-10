22f60abd40007e49bfbc1fdb4d982a33066bea8d
Ragudos <workemailaaronragudos@gmail.com>
2024-10-10 20:34:56 +0800

    add abstraction for the program's loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <signal.h>
 
+#include <functional>
 #include <iostream>
 #include <keyboard.hpp>
 #include <string>
@@ -8,8 +9,6 @@
 
 using namespace std;
 using namespace miscellaneous;
-
-bool endProgram = false;
 
 enum RendererState { SHOP, ABOUT, CART, CHECKOUT };
 
@@ -23,6 +22,12 @@ class Renderer {
    public:
     RendererState currentState;
 
+    /**
+     *
+     * This is a singleton pattern.
+     * We use this to allow us to access this from anywhere without having to
+     * pass this as an argument in functions.
+     */
     static Renderer& getInstance() {
         static Renderer instance;
         return instance;
@@ -31,45 +36,78 @@ class Renderer {
     bool render() { throw logic_error("unimplemented"); }
 };
 
-int main() {
-    while (!endProgram) {
-        int pressedKeyCode = getPressedKeyCode();
+class ProgramLoop {
+   private:
+    function<void(ProgramLoop*)> lambda;
+    /**
+     *
+     * Time before the next iteration is ran. This is to conserve CPU.
+     * This ProgramLoop class will use the same strategy as polling.
+     */
+    int waitTime;
+    bool shouldLoop;
 
-        switch (pressedKeyCode) {
-            case KEY_UP:
-                break;
-            case KEY_DOWN:
-                break;
-            case KEY_LEFT:
-                break;
-            case KEY_RIGHT:
-                break;
-            case KEY_A:
-            case KEY_a:
-                break;
-            case KEY_S:
-            case KEY_s:
-                break;
-            case KEY_C:
-            case KEY_c:
-                break;
-            case KEY_Q:
-            case KEY_q: {
-                // TODO: Cleanup and stuff should be done on exit
-                endProgram = true;
-            }; break;
-            case KEY_PLUS:
-                break;
-            case KEY_HYPHEN_MINUS:
-                break;
-            case KEY_ESC:
-                break;
-            case KEY_BACKSPACE:
-                break;
+   public:
+    ProgramLoop(const function<void(ProgramLoop*)> lambda, const int waitTime)
+        : lambda(lambda), waitTime(waitTime), shouldLoop(false){};
+
+    void start() {
+        if (shouldLoop) {
+            return;
         }
 
-        wait(100);
+        shouldLoop = true;
+
+        while (shouldLoop) {
+            lambda(this);
+            wait(waitTime);
+        }
     }
+
+    void stop() { shouldLoop = false; }
+};
+
+void programEntryPoint(ProgramLoop* programLoop) {
+    int pressedKeyCode = getPressedKeyCode();
+
+    switch (pressedKeyCode) {
+        case KEY_UP:
+            break;
+        case KEY_DOWN:
+            break;
+        case KEY_LEFT:
+            break;
+        case KEY_RIGHT:
+            break;
+        case KEY_A:
+        case KEY_a:
+            break;
+        case KEY_S:
+        case KEY_s:
+            break;
+        case KEY_C:
+        case KEY_c:
+            break;
+        case KEY_Q:
+        case KEY_q:
+            // TODO: Cleanup here
+            programLoop->stop();
+            break;
+        case KEY_PLUS:
+            break;
+        case KEY_HYPHEN_MINUS:
+            break;
+        case KEY_ESC:
+            break;
+        case KEY_BACKSPACE:
+            break;
+    }
+}
+
+int main() {
+    ProgramLoop programLoop(programEntryPoint, 100);
+
+    programLoop.start();
 
     return 0;
 }
