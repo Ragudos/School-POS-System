f005113a660a98607bfb815d2d92e830ef0c7da8
Ragudos <workemailaaronragudos@gmail.com>
2024-10-16 17:47:46 +0800

    add select functionality prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <iostream>
 #include <keyboard.hpp>
+#include <sstream>
 #include <string>
 #include <thread>
 #include <utils.hpp>
@@ -15,11 +16,11 @@ enum RendererState { SHOP, ABOUT, CART, CHECKOUT };
 class Renderer {
    private:
     Renderer() : currentState(SHOP){};
-
     Renderer(const Renderer&) = delete;
     Renderer& operator=(const Renderer&) = delete;
 
    public:
+    ostringstream buf;
     RendererState currentState;
 
     /**
@@ -33,7 +34,22 @@ class Renderer {
         return instance;
     }
 
-    bool render() { throw logic_error("unimplemented"); }
+    void render() {
+        cout << buf.str();
+        buf.str("");
+        buf.clear();
+    }
+
+    void clearArea(int fromRow, int fromCol, int height) {
+        buf << "\033[" << fromRow << ";" << fromCol << "H";
+
+        for (int i = 0; i < height; ++i) {
+            buf << "\033[K";
+            buf << "\033[B";
+        }
+
+        buf << "\033[" << fromRow << ";" << fromCol << "H";
+    }
 };
 
 class ProgramLoop {
@@ -67,14 +83,93 @@ class ProgramLoop {
     void stop() { shouldLoop = false; }
 };
 
+struct Choice {
+    int position;
+    string displayName;
+    string value;
+};
+
+class Menu {
+   private:
+    int chosenChoice = 1;
+    bool isValidChoice(int choice) { return choice < 5 && choice > 0; }
+
+    Menu(){};
+    Menu(const Menu&) = delete;
+    Menu& operator=(const Menu&) = delete;
+
+   public:
+    const int MAX_MENU_CHOICES = 4;
+    const Choice menuChoices[4] = {
+        {1, "Decaf Coffee", "decaf"},
+        {2, "Cold Brew", "cold-brew"},
+        {3, "Java Chip", "java-chip"},
+        {4, "Caramel Macchiato", "caramel-macchiato"}};
+
+    /**
+     *
+     * This is a singleton pattern.
+     * We use this to allow us to access this from anywhere without having to
+     * pass this as an argument in functions.
+     */
+    static Menu& getInstance() {
+        static Menu instance;
+        return instance;
+    }
+
+    bool setChosenChoice(int choice) {
+        if (!isValidChoice(choice)) {
+            return false;
+        }
+
+        chosenChoice = choice;
+
+        return true;
+    }
+
+    int getChosenChoice() { return chosenChoice; }
+
+    void displayChoices() {
+        Renderer& renderer = Renderer::getInstance();
+
+        for (int i = 0; i < MAX_MENU_CHOICES; ++i) {
+            Choice choice = menuChoices[i];
+
+            renderer.buf << choice.displayName;
+
+            if (choice.position == getChosenChoice()) {
+                renderer.buf << " <";
+            }
+
+            renderer.buf << endl;
+        }
+    }
+};
+
 void programEntryPoint(ProgramLoop* programLoop) {
+    Menu& menu = Menu::getInstance();
+    Renderer& renderer = Renderer::getInstance();
     int pressedKeyCode = getPressedKeyCode();
 
     switch (pressedKeyCode) {
-        case KEY_UP:
-            break;
-        case KEY_DOWN:
-            break;
+        case KEY_UP: {
+            if (menu.getChosenChoice() == 1) {
+                menu.setChosenChoice(menu.MAX_MENU_CHOICES);
+            } else {
+                menu.setChosenChoice(menu.getChosenChoice() - 1);
+            }
+            renderer.clearArea(0, 0, 5);
+            menu.displayChoices();
+        }; break;
+        case KEY_DOWN: {
+            if (menu.getChosenChoice() == menu.MAX_MENU_CHOICES) {
+                menu.setChosenChoice(1);
+            } else {
+                menu.setChosenChoice(menu.getChosenChoice() + 1);
+            }
+            renderer.clearArea(0, 0, 5);
+            menu.displayChoices();
+        }; break;
         case KEY_LEFT:
             break;
         case KEY_RIGHT:
@@ -90,7 +185,6 @@ void programEntryPoint(ProgramLoop* programLoop) {
             break;
         case KEY_Q:
         case KEY_q:
-            // TODO: Cleanup here
             programLoop->stop();
             break;
         case KEY_PLUS:
@@ -102,11 +196,17 @@ void programEntryPoint(ProgramLoop* programLoop) {
         case KEY_BACKSPACE:
             break;
     }
+
+    renderer.render();
 }
 
 int main() {
+    Menu& menu = Menu::getInstance();
+    Renderer& renderer = Renderer::getInstance();
     ProgramLoop programLoop(programEntryPoint, 100);
 
+    // Hide cursor
+    menu.displayChoices();
     programLoop.start();
 
     return 0;
