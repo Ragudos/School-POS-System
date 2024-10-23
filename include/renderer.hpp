6be7804158fa4c040d1e400da80e74c017746e08
Ragudos <workemailaaronragudos@gmail.com>
2024-10-23 16:09:59 +0800

    add prototype for navigation tab functionality

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -41,7 +41,49 @@ class Renderer {
     Renderer(const Renderer&) = delete;
     Renderer& operator=(const Renderer&) = delete;
 
+    void bufferHeroSection() {
+        moveCursorTo(1, 1);
+        buf << "\033[33m"
+            << "Comffee POS System"
+            << "\033[37m";
+    }
+
+    void bufferNav() {
+        for (int i = 0; i < 3; ++i) {
+            Tab& tab = navigationTabs[i];
+            int prevTabWidth = 1;
+
+            if (i != 0) {
+                Tab prevTab = navigationTabs[i - 1];
+                prevTabWidth = prevTab.pos[0] + prevTab.dimensions[0] + (4 * i);
+            }
+
+            moveCursorTo(tab.pos[1], prevTabWidth);
+            buf << "\033[1m" << (char)tab.keyCodes[0] << "\033[0m";
+            moveCursorTo(tab.pos[1], prevTabWidth + 2);
+
+            if (!tab.isActive) {
+                buf << "\033[2m";
+            } else {
+                buf << "\033[1m";
+            }
+
+            buf << tab.text;
+
+            if (!tab.isActive) {
+                buf << "\033[0m";
+            }
+        }
+    }
+
    public:
+    /** The row and below that can isn't controlled by this Renderer */
+    const int DYNAMIC_STARTING_ROW = 6;
+    Tab navigationTabs[3] = {
+        {true, {1, 3}, {4, 1}, "shop", {KEY_s, KEY_S}},
+        {false, {5, 3}, {5, 1}, "admin", {KEY_a, KEY_A}},
+        {false, {10, 3}, {8, 1}, "checkout", {KEY_c, KEY_C}},
+    };
     ostringstream buf;
     RendererState currentState;
 
@@ -57,19 +99,48 @@ class Renderer {
     }
 
     void render() {
+        bufferHeroSection();
+        bufferNav();
+
         cout << buf.str();
         buf.str("");
         buf.clear();
     }
 
+    void changeNavTab(int key) {
+        for (int i = 0; i < 3; ++i) {
+            Tab& tab = navigationTabs[i];
+
+            if (tab.isActive) {
+                if (tab.keyCodes[0] == key || tab.keyCodes[1] == key) {
+                    return;
+                }
+
+                tab.isActive = false;
+            } else if (tab.keyCodes[0] == key || tab.keyCodes[1] == key) {
+                tab.isActive = true;
+            }
+        }
+
+        render();
+    }
+
+    void moveCursorTo(int row, int col, bool buffer = true) {
+        if (buffer) {
+            buf << "\033[" << row << ";" << col << "H";
+        } else {
+            cout << "\033[" << row << ";" << col << "H";
+        }
+    }
+
     void clearArea(int fromRow, int fromCol, int height) {
-        buf << "\033[" << fromRow << ";" << fromCol << "H";
+        moveCursorTo(fromRow, fromCol);
 
         for (int i = 0; i < height; ++i) {
             buf << "\033[K";
             buf << "\033[B";
         }
 
-        buf << "\033[" << fromRow << ";" << fromCol << "H";
+        moveCursorTo(fromRow, fromCol);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,14 @@ void programEntryPoint(LoopLambda* loop) {
             break;
         case KEY_A:
         case KEY_a:
-            break;
         case KEY_S:
         case KEY_s:
-            break;
         case KEY_C:
         case KEY_c:
+            // TODO: Check if we can go to checkout
+            // (condition is that there should be items
+            // in the cart).
+            renderer.changeNavTab(pressedKeyCode);
             break;
         case KEY_Q:
         case KEY_q:
@@ -42,14 +44,37 @@ void programEntryPoint(LoopLambda* loop) {
             break;
     }
 
-    renderer.render();
+    /**
+     * Sample to get input from user
+     *
+     * renderer.moveCursorTo(renderer.DYNAMIC_STARTING_ROW, 1);
+        renderer.buf << "Enter your name: ";
+
+        renderer.render();
+
+        renderer.moveCursorTo(renderer.DYNAMIC_STARTING_ROW, 18, false);
+
+        string in;
+        cin >> in;
+
+        renderer.moveCursorTo(renderer.DYNAMIC_STARTING_ROW, 1, false);
+        cout << "\033[K";
+
+        cout << "Hello, " << in;
+    */
 }
 
 int main() {
     // Reset the terminal so cursor gets reset
     clearTerminal();
 
-    LoopLambda loop(100, programEntryPoint);
+    Renderer& renderer = Renderer::getInstance();
+
+    // Disable text wrapping
+    renderer.buf << "\e[?7l";
+    renderer.render();
+
+    LoopLambda loop(10, programEntryPoint);
 
     loop.start();
 
